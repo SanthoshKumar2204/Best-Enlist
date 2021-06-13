2d8048f94bb226ae7536526c0f1a1c3fda488ed7
SanthoshKumar2204 <85781693+SanthoshKumar2204@users.noreply.github.com>
2021-06-13 13:06:12 +0530

    Update and rename Santhosh-task-day2 to Santhosh-task-day2.py

---
+++ Santhosh-task-day2.py
@@ -0,0 +1,76 @@
+#Day 2 -Bootcamp task
+
+#print a value
+print("30 days 30 hour challenge")
+print('30 days 30 hour challenge')
+
+#String to Variable
+Hours = "thirty"
+print(Hours)
+
+#Indexing using String:
+Days = "Thirty days"
+print(Days[0])
+
+#print the particular character from certain text
+Challenge = "I will win"
+print(Challenge[7:10])
+
+#Print the length of Character:
+Challenge = "I will win"
+print(len(Challenge))
+
+#Convert String into lower character:
+Challenge = "I will win"
+print(Challenge.lower())
+
+#String Concatenation
+a = "30 Days"
+b = "30 hours"
+c = a + b
+print(c)
+
+#Adding space during concatenation 
+a = "30 Days"
+b = "30 hours"
+c = a + " " + b
+print(c)
+
+#casefold()
+text = "Thirty days and Thirty hours"
+x = text.casefold()
+print(x)
+
+#capitalize()
+text = "Thirty days and Thirty hours"
+x = text.capitalize()
+print(x)
+
+#find()
+text = "Thirty days and Thirty hours"
+x = text.find('r')
+print(x)
+
+#isalpha()
+text = "Thirty days and Thirty hours"
+x = text.isalpha()
+print(x)
+
+#isalnum()
+text = "Thirty days and Thirty hours"
+x = text.isalnum()
+print(x)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
